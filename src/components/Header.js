6693d119295e1d81a909916c45318d2223fc2271
AngRafFrancavilla <francavillaangeloraffaele@gmail.com>
2025-03-05 15:47:40 +0100

    aggiunta icon

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+  /* import "../data/img/logo.jpeg"
+
+function Header() {
+    return (
+      <header style={{ display: "flex", alignItems: "center", gap: "10px", padding: "10px" }}>
+        <img src="logo.jpeg" alt="Logo" style={{ width: "30px", height: "30px" }} />
+        <h1>Raffaele Francavilla - Portfolio</h1>
+      </header>
+    );
+  }
+  
+  export default Header;
+*/
